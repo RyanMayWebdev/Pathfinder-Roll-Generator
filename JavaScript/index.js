e1aa6ab9f44ddb6fc83a84c31f938e79777f28d0
RyanMayWebdev <83085214+RyanMayWebdev@users.noreply.github.com>
2021-05-19 17:54:48 -0400

    Added new features
    Added the ability to use multiple different dice combos in one roll. Added rolls for concentration checks and saving throws. Added new stat options related to these new kinds of rolls.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -12,6 +12,10 @@ let chaMod = 0;
 let conMod = 0;
 let wisMod = 0;
 let bab = 0;
+let casterLevel = 0;
+let baseSaveFort = 0;
+let baseSaveReflex = 0;
+let baseSaveWill = 0;
 let ac = 0;
 let acPenalty = 0;
 let skill = 0;
@@ -29,18 +33,15 @@ const wisElement = document.getElementById("Wisdom");
 document.getElementById("rollBtn").addEventListener("click", rollGenerator);
 document.getElementById("submitBtn").addEventListener("click", storeSkills);
 document.getElementById("editCharacter").addEventListener("click", editSkills);
-
 // Reset all fields when reset button is clicked
 document.getElementById("reset").addEventListener("click", () => {
     localStorage.clear();
     storedData = "false"
-
     let inputFields = document.querySelectorAll("input");
-    for (let x = 0; x < inputFields.length; x++) {
-        inputFields[x].value = 0;
+    for (let field of inputFields) {
+        field.value = 0;
     }
     characterSubmitted();
-
 });
 
 //Checks for locally stored character data
@@ -71,12 +72,16 @@ function getValues() {
     conMod = Math.floor((con - 10) / 2);
     wisMod = Math.floor((wis - 10) / 2);
     bab = parseInt(document.getElementById("bab").value);
+    casterLevel = parseInt(document.getElementById("casterLevel").value);
+    baseSaveFort = parseInt(document.getElementById("baseSaveFort").value);
+    baseSaveReflex = parseInt(document.getElementById("baseSaveReflex").value);
+    baseSaveWill = parseInt(document.getElementById("baseSaveWill").value);
     ac = parseInt(document.getElementById("ac").value);
     acPenalty = parseInt(document.getElementById("acPenalty").value);
     skill = document.querySelectorAll(".skill");
     skillValue = [];
-    for (let x = 0; x < skill.length; x++) {
-        skillValue.push(parseInt(skill[x].value));
+    for (let index of skill) {
+        skillValue.push(parseInt(index.value));
     }
 }
 
@@ -96,6 +101,10 @@ function storeSkills() {
     localStorage.setItem("conMod", conMod);
     localStorage.setItem("wisMod", wisMod);
     localStorage.setItem("bab", bab);
+    localStorage.setItem("casterLevel",casterLevel);
+    localStorage.setItem("baseSaveFort", baseSaveFort);
+    localStorage.setItem("baseSaveReflex", baseSaveReflex);
+    localStorage.setItem("baseSaveWill", baseSaveWill);
     localStorage.setItem("ac", ac);
     localStorage.setItem("acPenalty", acPenalty);
     localStorage.setItem("skillValue", JSON.stringify(skillValue));
@@ -113,6 +122,10 @@ function editSkills() {
     conElement.value = localStorage.getItem("con");
     wisElement.value = localStorage.getItem("wis");
     document.getElementById("bab").value = localStorage.getItem("bab");
+    document.getElementById("casterLevel").value = localStorage.getItem("casterLevel");
+    document.getElementById("baseSaveFort").value = localStorage.getItem("baseSaveFort");
+    document.getElementById("baseSaveReflex").value = localStorage.getItem("baseSaveReflex");
+    document.getElementById("baseSaveWill").value = localStorage.getItem("baseSaveWill");
     document.getElementById("ac").value = localStorage.getItem("ac");
     document.getElementById("acPenalty").value = localStorage.getItem("acPenalty");
     skill = document.querySelectorAll(".skill");
@@ -126,8 +139,31 @@ function editSkills() {
 }
 
 function rollGenerator() {
-    //get values from the form fields and assign them to their respective skill
+    // collect dice input from each input field the user adds. then get how many dice of each type are rolled and assign the diceSides and numberOfDice
+    let userDiceInput = document.getElementById("diceRoll").value;
+    let numberOfDiffDice = userDiceInput.split('+').length; //Checks if the user is combining different dice types and how many
+    let d = []; // an array to hold the indexOf the character 'd'
+    let seperateDice; // variable that will contain the indexOf '+', used to seperate the different dice being used
+    let numberOfDice = []; // array to hold how many dice will be rolled per type of dice
+    let diceSides = []; //array to hold the number of sides per dice type being rolled
+    for (let x = 0; x < numberOfDiffDice; x++) { 
+        seperateDice = userDiceInput.indexOf('+'); // checks for '+' within the input and if so grabs its location
+        d.push(userDiceInput.indexOf('d' || 'D')); // checks for location of 'd', used to determine when numberofsides should be collected
+        numberOfDice.push(parseInt(userDiceInput.slice(0, d[x]))); //grabs whatever proceeds 'd' to be used as the number of dice
+        if (seperateDice > 0) {  //logic check to see if there is more than 1 type of dice being used.
+            diceSides.push(parseInt(userDiceInput.slice(d[x] + 1, seperateDice)));
+        } else {
+            diceSides.push(parseInt(userDiceInput.slice(d[x] + 1)));
+        }
+        userDiceInput = userDiceInput.slice(seperateDice + 1); //Removes previously checked dice data from string so that it isn't checked
+    }
+    let diceRoll = [];
+    let ranNum = 0;
+    let finalRoll = 0;
+    let outcome = document.getElementById("rollOutcome");
 
+    //get values from the form fields and assign them to their respective skill
+    outcome.textContent = "";
     if (storedData === "true") {
         dexMod = parseInt(localStorage.getItem("dexMod"));
         strengthMod = parseInt(localStorage.getItem("strengthMod"));
@@ -136,6 +172,10 @@ function rollGenerator() {
         conMod = parseInt(localStorage.getItem("conMod"));
         wisMod = parseInt(localStorage.getItem("wisMod"));
         bab = parseInt(localStorage.getItem("bab"));
+        casterLevel = parseInt(localStorage.getItem("casterLevel"));
+        baseSaveFort = parseInt(localStorage.getItem("baseSaveFort"));
+        baseSaveReflex = parseInt(localStorage.getItem("baseSaveReflex"));
+        baseSaveWill = parseInt(localStorage.getItem("baseSaveWill"));
         ac = parseInt(localStorage.getItem("ac"));
         acPenalty = parseInt(localStorage.getItem("acPenalty"));
         skillValue = JSON.parse(localStorage.getItem("skillValue"));
@@ -147,8 +187,14 @@ function rollGenerator() {
     const atrValue = {
         attackDex: dexMod + bab,
         attackStr: strengthMod + bab,
-        damageDex: dexMod,
-        damageStr: strengthMod,
+        concentrationCha : casterLevel + chaMod,
+        concentrationIntel : casterLevel + intelMod,
+        concentrationWis : casterLevel + wisMod,
+        damageDex : dexMod,
+        damageStr : strengthMod,
+        fortitudeSave : baseSaveFort + conMod,
+        reflexSave : baseSaveReflex + dexMod,
+        willSave : baseSaveWill + wisMod,
         acrobatics: skillValue[0] + dexMod - acPenalty,
         appraise: skillValue[1] + intelMod,
         bluff: skillValue[2] + chaMod,
@@ -178,30 +224,32 @@ function rollGenerator() {
 
     }
 
-    // parse ints from the diceroll id. need to check for number up until d and assign to numberofdice var, numbers after d are assigned to dicesides var.
-    let userDiceInput = document.getElementById("diceRoll").value;
-    let d = userDiceInput.indexOf('d' || 'D');
-    let numberOfDice = userDiceInput.slice(0, d);
-    let diceSides = userDiceInput.slice(d + 1);
-    let diceRoll = [];
-    let ranNum = 0;
-    let finalRoll = 0;
-    let outcome = document.getElementById("rollOutcome");
-    // Randomizes dice rolls * the number of rolls required using the number of dice sides
-    for (let x = 0; x < numberOfDice; x++) {
-        ranNum = Math.floor( (Math.random() * diceSides )+ 1);
-        diceRoll.push(ranNum)
-        finalRoll = finalRoll + diceRoll[x];
+    // Randomizes dice rolls * the number of rolls required using the number of dice sides per each different kind of dice entered
+    let i = 0;
+ 
+    while (i < numberOfDiffDice) {
+        for (let x = 0; x < numberOfDice[i]; x++) {
+            ranNum = Math.floor((Math.random() * diceSides[i]) + 1);
+            diceRoll.push(ranNum)
+            finalRoll = finalRoll + diceRoll[x];
+        }
+        i++
     }
     finalRoll += atrValue[atrUsed];
-    // roll outcome logic statments
-    if (atrUsed === "damageDex" || atrUsed === "damageStr") {
-        outcome.textContent = `Roll was: ${diceRoll} + Modifier : ${atrValue[atrUsed]} = final of: ${finalRoll}`;
-    } else if (ranNum == 20){
-        outcome.textContent = `Natural 20 crit! Roll was: ${diceRoll} + Modifier : ${atrValue[atrUsed]} = final of: ${finalRoll}`;
-    }else if (ranNum === 1){
-        outcome.textContent = `Critical Failure! Roll was: ${diceRoll} + Modifier : ${atrValue[atrUsed]} = final of: ${finalRoll}`;
-    }else{
-        outcome.textContent = `Roll was: ${diceRoll} + Modifier : ${atrValue[atrUsed]} = final of: ${finalRoll}`;
-    }
+    let diceAudio = new Audio("audio/diceroll.mp3");
+    diceAudio.play();
+    // roll outcome logic statments, using 1.5 second delay to allow diceRoll audio to play  before displaying result.
+    setTimeout(() => {
+        if (atrUsed === "damageDex" || atrUsed === "damageStr") {
+            outcome.textContent = `Roll was: ${diceRoll} + Modifier : ${atrValue[atrUsed]} = final of: ${finalRoll}`;
+        } else if (ranNum == 20) {
+            outcome.textContent = `Natural 20 crit! Roll was: ${diceRoll} + Modifier : ${atrValue[atrUsed]} = final of: ${finalRoll}`;
+        } else if (ranNum === 1) {
+            outcome.textContent = `Critical Failure! Roll was: ${diceRoll} + Modifier : ${atrValue[atrUsed]} = final of: ${finalRoll}`;
+        } else {
+            outcome.textContent = `Roll was: ${diceRoll} + Modifier : ${atrValue[atrUsed]} = final of: ${finalRoll}`;
+        }
+    }, 1500);
+
+
 }
